a006f82a901c393caaf552e137563fc84e0551f1
anh.do <anh.do@sutrixsolutions.com>
2017-12-11 15:46:38 +0700

    update code js

--- js/scripts.js
+++ js/scripts.js
@@ -268,13 +268,4 @@ gapi.analytics.ready(function() {
     return div.innerHTML;
   }
 });
-function testAPI() {
-  FB.api(
-  '/me', 'GET', {
-    "fields":"id, name, email"
-  },
-  function(response) {
-    document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
-    console.log(response);
-  });
-}
+
